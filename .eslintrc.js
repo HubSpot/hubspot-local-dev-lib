1020e34d82bb64a801309bc44373f343d8cea367
Camden Phalen <cphalen@hubspot.com>
2023-02-27 16:36:45 -0500

    add style config files

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-return-await': 'error',
+  },
+
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.js', '**/__mocks__/**/*.js'],
+      env: {
+        jest: true,
+        node: true,
+      },
+    },
+    {
+      files: ['acceptance-tests/tests/**/*.js'],
+      env: {
+        jasmine: true,
+        node: true,
+      },
+    },
+  ],
+};
