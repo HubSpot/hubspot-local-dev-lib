4f560e29fa916abca1b158565da1df5f25a68f2e
Camden Phalen <cphalen@hubspot.com>
2023-02-27 17:09:35 -0500

    Add scripts and dev dependancies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     browser: false,
@@ -8,24 +8,25 @@ module.exports = {
     es6: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': 'off',
     'no-return-await': 'error',
   },
-
   overrides: [
     {
-      files: ['**/__tests__/**/*.js', '**/__mocks__/**/*.js'],
+      files: ['**/__tests__/**/*.ts', '**/__mocks__/**/*.ts'],
       env: {
         jest: true,
         node: true,
       },
     },
     {
-      files: ['acceptance-tests/tests/**/*.js'],
+      files: ['acceptance-tests/tests/**/*.ts'],
       env: {
         jasmine: true,
         node: true,
