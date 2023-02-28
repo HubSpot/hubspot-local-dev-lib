628cb538af36a27b0ee837f53de7dbdb17c1a914
Camden Phalen <cphalen@hubspot.com>
2023-02-28 11:28:00 -0500

    Set up new version of husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2022,
   },
   plugins: ['@typescript-eslint'],
   rules: {
--- src/test-husky.ts
+++
@@ -1,3 +0,0 @@
-export function test(str: string): string {
-  return `${str}test`;
-}
