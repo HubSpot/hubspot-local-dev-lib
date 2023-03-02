9acdbd9d2f67c8826aefbece56c7a069702aa541
Camden Phalen <cphalen@hubspot.com>
2023-03-02 16:07:15 -0500

    move a few utils over

---
+++ src/utils/environment.ts
@@ -0,0 +1,19 @@
+import { ENVIRONMENTS } from '../constants/environments';
+
+export function getValidEnv(
+  env: string,
+  maskedProductionValue?: string
+): string {
+  const prodValue = maskedProductionValue
+    ? maskedProductionValue
+    : ENVIRONMENTS.PROD;
+
+  const returnVal =
+    typeof env &&
+    typeof env === 'string' &&
+    env.toLowerCase() === ENVIRONMENTS.QA
+      ? ENVIRONMENTS.QA
+      : prodValue;
+
+  return returnVal;
+}
