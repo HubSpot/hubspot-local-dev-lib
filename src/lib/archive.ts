6f039f64060b44a0220517b55474e1d5edc8edfd
Camden Phalen <cphalen@hubspot.com>
2023-03-09 15:37:42 -0500

    more archive/lang stuff

--- src/lib/archive.ts
+++ src/lib/archive.ts
@@ -6,13 +6,9 @@ import __extract from 'extract-zip';
 
 import { throwFileSystemError } from '../errors/fileSystemErrors';
 import { throwError } from '../errors/standardErrors';
-import { log } from '../utils/logs';
+import { log, debug } from '../utils/logs';
 import { LogCallbacks } from '../types/LogCallbacks';
 import { BaseError } from '../types/Error';
-
-const extractZipCallbackKeys = ['onInit'] as const;
-type ExtractZipCallbackKeys = (typeof extractZipCallbackKeys)[number];
-type ExtractZipLogCallbacks = LogCallbacks<ExtractZipCallbackKeys>;
 
 const extract = promisify(__extract);
 
@@ -20,6 +16,10 @@ type ZipData = {
   extractDir: string;
   tmpDir: string;
 };
+
+const extractZipCallbackKeys = ['onInit'] as const;
+type ExtractZipCallbackKeys = (typeof extractZipCallbackKeys)[number];
+type ExtractZipLogCallbacks = LogCallbacks<ExtractZipCallbackKeys>;
 
 async function extractZip(
   name: string,
@@ -29,10 +29,14 @@ async function extractZip(
   const result: ZipData = { extractDir: '', tmpDir: '' };
 
   const TMP_FOLDER_PREFIX = `hubspot-temp-${name}-`;
-  log<ExtractZipCallbackKeys>('onInit', logCallbacks);
+  log<ExtractZipCallbackKeys>(
+    'onInit',
+    logCallbacks,
+    'archive.extractZip.init'
+  );
 
   // Write zip to disk
-  let tmpZipPath;
+  let tmpZipPath = '';
   try {
     result.tmpDir = await fs.mkdtemp(join(tmpdir(), TMP_FOLDER_PREFIX));
     tmpZipPath = join(result.tmpDir, 'hubspot-temp.zip');
@@ -41,7 +45,7 @@ async function extractZip(
       mode: 0o777,
     });
   } catch (err) {
-    console.debug('An error occured writing temp project source.');
+    debug('archive.extractZip.writeError');
     if (tmpZipPath || result.tmpDir) {
       throwFileSystemError(err as BaseError, {
         filepath: tmpZipPath || result.tmpDir,
@@ -58,34 +62,42 @@ async function extractZip(
     await extract(tmpZipPath, { dir: tmpExtractPath });
     result.extractDir = tmpExtractPath;
   } catch (err) {
-    console.debug('An error occured extracting project source.');
+    debug('archive.extractZip.extractError');
     throwError(err as BaseError);
-    return result;
   }
-  console.debug('Completed project source extraction.');
+  debug('archive.extractZip.success');
   return result;
 }
 
-/**
- * @param {String} src - Dir where boilerplate repo files have been extracted.
- * @param {String} sourceDir - Directory in project that should get copied.
- * @param {String} dest - Dir to copy boilerplate src files to.
- * @returns {Boolean} `true` if successfully copied, `false` otherwise.
- */
+type CopySourceToDestOptions = {
+  sourceDir?: string;
+  includesRootDir?: boolean;
+};
+
+const copySourceToDestCallbackKeys = ['onInit'] as const;
+type CopySourceToDestCallbackKeys =
+  (typeof copySourceToDestCallbackKeys)[number];
+type CopySourceToDestCallbacks = LogCallbacks<ExtractZipCallbackKeys>;
+
 async function copySourceToDest(
-  src,
-  dest,
-  { sourceDir = null, includesRootDir = true } = {}
-) {
+  src: string,
+  dest: string,
+  { sourceDir, includesRootDir = true }: CopySourceToDestOptions = {},
+  logCallbacks?: CopySourceToDestCallbacks
+): Promise<boolean> {
   try {
-    logger.log('Copying project source...');
+    log<CopySourceToDestCallbackKeys>(
+      'onInit',
+      logCallbacks,
+      'archive.copySourceToDest.init'
+    );
     const srcDirPath = [src];
 
     if (includesRootDir) {
       const files = await fs.readdir(src);
       const rootDir = files[0];
       if (!rootDir) {
-        logger.debug('Project source is empty');
+        debug('archive.copySourceToDest.sourceEmpty');
         // Create the dest path if it doesn't already exist
         fs.ensureDir(dest);
         // No root found so nothing to copy
@@ -101,11 +113,11 @@ async function copySourceToDest(
     const projectSrcDir = join(...srcDirPath);
 
     await fs.copy(projectSrcDir, dest);
-    logger.debug('Completed copying project source.');
+    debug('archive.copySourceToDest.success');
     return true;
   } catch (err) {
-    logger.error(`An error occured copying project source to ${dest}.`);
-    logFileSystemErrorInstance(err, {
+    debug('archive.copySourceToDest.error', { dest });
+    throwFileSystemError(err as BaseError, {
       filepath: dest,
       write: true,
     });
---
+++ src/utils/interpolation.ts
@@ -0,0 +1,54 @@
+const delimiters = {
+  interpolation: {
+    start: '{{',
+    end: '}}',
+  },
+};
+
+function generateReplaceFn(
+  matchedText: string,
+  startIndex: number,
+  replacementString: string
+): (currentStringValue: string) => string {
+  return function (currentStringValue: string): string {
+    return `${currentStringValue.slice(0, startIndex)}${
+      replacementString || ''
+    }${currentStringValue.slice(startIndex + matchedText.length)}`;
+  };
+}
+
+type InterpolationData = {
+  [indentifier: string]: string;
+};
+
+export function interpolate(
+  stringValue: string,
+  interpolationData: InterpolationData
+): string {
+  const interpolationIdentifierRegEx = new RegExp(
+    `${delimiters.interpolation.start}(.*?)${delimiters.interpolation.end}`,
+    'g'
+  );
+  const replaceQueue = [];
+  let match: RegExpExecArray | null;
+
+  // while & reduce necessary because RegExp.exec is stateful and only runs
+  // from beginning to end of string
+  while ((match = interpolationIdentifierRegEx.exec(stringValue)) != null) {
+    const { 0: matchedText, 1: rawIdentifier, index } = match;
+    const identifier = rawIdentifier.trim();
+
+    if (identifier) {
+      replaceQueue.unshift(
+        generateReplaceFn(matchedText, index, interpolationData[identifier])
+      );
+    }
+  }
+
+  const compiledString = replaceQueue.reduce(
+    (currentValue, replaceFn) => replaceFn(currentValue),
+    stringValue
+  );
+
+  return compiledString;
+}
