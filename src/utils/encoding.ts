cea32f494f7d0606bc61b4a1e629e7a22ed20e73
Camden Phalen <cphalen@hubspot.com>
2023-05-02 16:16:52 -0400

    delete encoding, move files.ts to cms/themes.ts

--- src/utils/encoding.ts
+++
@@ -1,8 +0,0 @@
-export function base64EncodeString(valueToEncode: string): string {
-  if (typeof valueToEncode !== 'string') {
-    return valueToEncode;
-  }
-
-  const stringBuffer = Buffer.from(valueToEncode);
-  return encodeURIComponent(stringBuffer.toString('base64'));
-}
