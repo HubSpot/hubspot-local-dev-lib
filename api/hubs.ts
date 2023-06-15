871db5a245bfad31751d52ba5bb6a78b384a15d3
Camden Phalen <cphalen@hubspot.com>
2023-06-15 17:10:10 -0400

    port hubs

---
+++ api/hubs.ts
@@ -0,0 +1,30 @@
+import request from 'request-promise-native';
+import { getRequestOptions } from '../http/requestOptions';
+import { ENVIRONMENTS } from '../constants/environments';
+import { ValueOf } from '../types/Utils';
+
+const SANDBOX_HUBS_API_PATH = 'sandbox-hubs/v1/self';
+
+export async function fetchSandboxHubData(
+  accessToken: string,
+  portalId: number,
+  env: ValueOf<typeof ENVIRONMENTS> = ENVIRONMENTS.PROD
+) {
+  const requestOptions = getRequestOptions(
+    { env },
+    {
+      uri: SANDBOX_HUBS_API_PATH,
+      qs: { portalId },
+    }
+  );
+  const reqWithToken = {
+    ...requestOptions,
+    headers: {
+      ...requestOptions.headers,
+      Authorization: `Bearer ${accessToken}`,
+    },
+    url: SANDBOX_HUBS_API_PATH,
+  };
+
+  return request.get(reqWithToken);
+}
