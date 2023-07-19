b363c59cac1aa1875d9693c4b60ed358d6d02a33
Adam Wang <awang@hubspot.com>
2023-07-19 13:54:39 -0400

    Update HubSpotAuthError class

--- errors/HubSpotAuthError.ts
+++ errors/HubSpotAuthError.ts
@@ -1,6 +1,16 @@
+import { FullResponse } from 'request-promise-native';
+
 export class HubSpotAuthError extends Error {
-  constructor(message: string) {
+  statusCode?: number;
+  category?: string;
+  subCategory?: string;
+  constructor(message: string, errorResponse: Partial<FullResponse> = {}) {
     super(message);
     this.name = 'HubSpotAuthError';
+    this.statusCode = errorResponse && errorResponse.statusCode;
+    this.category =
+      (errorResponse.body && errorResponse.body.category) || undefined;
+    this.subCategory =
+      (errorResponse.body && errorResponse.body.subCategory) || undefined;
   }
 }
