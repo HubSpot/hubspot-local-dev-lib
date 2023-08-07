b44734d7e7f360ff895cbc2e2b1190c85e867c76
Allison Kemmerle <akemmerle@hubspot.com>
2023-08-07 17:05:42 -0400

    Add type for fetching platform version

--- api/developerProjects.ts
+++ api/developerProjects.ts
@@ -2,13 +2,14 @@ import http from '../http';
 
 const DEVELOPER_PROJECTS_API_PATH = 'developer/projects/v1';
 
-/**
- * Fetch default project platform version along with list of active platform versions
- *
- * @async
- * @returns {Promise}
- */
-export async function fetchPlatformVersions(accountId: number) {
+type FetchPlatformVersionResponse = {
+  defaultPlatformVersion: string;
+  activePlatformVersions: Array<string>;
+};
+
+export async function fetchPlatformVersions(
+  accountId: number
+): Promise<FetchPlatformVersionResponse> {
   return http.get(accountId, {
     uri: `${DEVELOPER_PROJECTS_API_PATH}/platformVersion`,
   });
--- lib/projectPlatformVersion.ts
+++ lib/projectPlatformVersion.ts
@@ -2,6 +2,5 @@ import { fetchPlatformVersions } from '../api/developerProjects';
 
 export const fetchDefaultVersion = async (accountId: number) => {
   const platformVersions = await fetchPlatformVersions(accountId);
-  // @ts-expect-error Property 'defaultPlatformVersion' does not exist on type 'Response'.
   return platformVersions.defaultPlatformVersion;
 };
