294ca5c90c0b0dbb039c4b13bdce171316b04ac5
Allison Kemmerle <akemmerle@hubspot.com>
2023-08-30 15:47:32 -0400

    Fix Validation type

--- types/HublValidation.ts
+++ types/HublValidation.ts
@@ -28,10 +28,10 @@ export type Validation = {
     all_widgets: Array<string>;
     widgets_in_rich_text: Array<string>;
     editable_flex_areas: Array<string>;
-    editable_layout_sections?: null; //TODO
-    email_style_settings: null;
+    editable_layout_sections: { key: string } | null;
+    email_style_settings: { key: string } | null;
     sms_flex_area: Array<string>;
-    google_font_variations?: null; //TODO
+    google_font_variations: { key: string } | null;
     custom_font_variations: Array<string>;
     has_style_tag: boolean;
     has_header_tag: boolean;
@@ -39,11 +39,11 @@ export type Validation = {
     has_menu_tag: boolean;
     has_theme_setting_function: boolean;
     template_source: string;
-    attribute_defaults?: null; //TODO
+    attribute_defaults?: { key: string } | null;
     template_errors: Array<HubLValidationError>;
     path_dependencies: Array<string>;
     theme_field_dependencies: Array<string>;
-    template_type_ids?: null; //TODO
+    template_type_ids?: { key: string } | null;
     exact_path_references: Array<string>;
     required_scopes_to_render: Array<string>;
   };
