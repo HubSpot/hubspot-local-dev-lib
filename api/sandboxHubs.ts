2931ff63f758bb0e618cf44c182e2580b5a41db6
Allison Kemmerle <akemmerle@hubspot.com>
2023-09-11 12:17:12 -0400

    Port sandbox hubs API endpoints and types

--- api/sandboxHubs.ts
+++ api/sandboxHubs.ts
@@ -1,10 +1,41 @@
 import axios from 'axios';
+import http from '../http';
 import { getAxiosConfig } from '../http/getAxiosConfig';
 import { ENVIRONMENTS } from '../constants/environments';
 import { Environment } from '../types/Config';
-import { SandboxHubData } from '../types/Sandbox';
+import { Sandbox, SandboxHubData, SandboxUsageLimits } from '../types/Sandbox';
 
-const SANDBOX_HUBS_API_PATH = 'sandbox-hubs/v1/self';
+const SANDBOX_API_PATH = 'sandbox-hubs/v1';
+const SANDBOX_API_PATH_V2 = 'sandbox-hubs/v2';
+
+export async function createSandbox(
+  accountId: number,
+  name: string,
+  type: string
+): Promise<Sandbox> {
+  return http.post(accountId, {
+    body: { name, type, generatePersonalAccessKey: true }, // For CLI, generatePersonalAccessKey will always be true since we'll be saving the entry to the config
+    timeout: 60000,
+    url: SANDBOX_API_PATH_V2, // Create uses v2 for sandbox type and PAK generation support
+  });
+}
+
+export async function deleteSandbox(
+  parentAccountId: number,
+  sandboxAccountId: number
+): Promise<void> {
+  return http.delete(parentAccountId, {
+    url: `${SANDBOX_API_PATH}/${sandboxAccountId}`,
+  });
+}
+
+export async function getSandboxUsageLimits(
+  parentAccountId: number
+): Promise<SandboxUsageLimits> {
+  return http.get(parentAccountId, {
+    url: `${SANDBOX_API_PATH}/parent/${parentAccountId}/usage`,
+  });
+}
 
 export async function fetchSandboxHubData(
   accessToken: string,
@@ -13,7 +44,7 @@ export async function fetchSandboxHubData(
 ): Promise<SandboxHubData> {
   const axiosConfig = getAxiosConfig({
     env,
-    url: `${SANDBOX_HUBS_API_PATH}`,
+    url: `${SANDBOX_API_PATH}/self`,
     params: { portalId },
   });
   const reqWithToken = {
--- types/Sandbox.ts
+++ types/Sandbox.ts
@@ -2,3 +2,133 @@ export type SandboxHubData = {
   type?: string | null;
   parentHubId?: number;
 };
+
+type User = {
+  userId: number;
+  firstName: string;
+  lastName: string;
+  gdprDeleted: boolean;
+  removed: boolean;
+  deactivated: boolean;
+};
+
+type TaskError = {
+  message: string;
+  in: string;
+  code: string;
+  subCategory: string;
+  context: {
+    missingScopes: Array<string>;
+  };
+};
+
+type MutationError = {
+  message: string;
+  portableKey: string;
+  subResource: {
+    type: string;
+    key: string;
+  };
+  standardError: {
+    status: string;
+    id: string;
+    category: string;
+    subCategory: { [key: string]: string };
+    message: string;
+    errors: Array<TaskError>;
+    context: {
+      additionalProp1: Array<string>;
+      additionalProp2: Array<string>;
+      additionalProp3: Array<string>;
+    };
+    links: {
+      additionalProp1: string;
+      additionalProp2: string;
+      additionalProp3: string;
+    };
+  };
+};
+
+type TaskStatus = {
+  numRequests: number;
+  numSuccesses: number;
+  errors: Array<TaskError>;
+  mutationErrors: Array<MutationError>;
+};
+
+type Task = {
+  id: string;
+  parentHubId: number;
+  sandboxHubId: number;
+  fromHubId: number;
+  toHubId: number;
+  command: string;
+  type: string;
+  status: string;
+  result: string;
+  sandboxType: string;
+  requestedAt: string;
+  requestedByUserId: number;
+  requestedByUser: User;
+  startedAt: string;
+  completedAt: string;
+  error: MutationError;
+  creates: TaskStatus;
+  updates: TaskStatus;
+  deletes: TaskStatus;
+  diffSummary: string;
+  portableKeys: Array<string>;
+};
+
+export type Sandbox = {
+  sandbox: {
+    sandboxHubId: number;
+    parentHubId: number;
+    createdAt: string;
+    updatedAt: string | null;
+    archivedAt: string | null;
+    type: string;
+    archived: boolean;
+    name: string;
+    domain: string;
+    createdByUser: User;
+    updatedByUser: User | null;
+    lastSync: {
+      id: string;
+      parentHubId: number;
+      sandboxHubId: number;
+      fromHubId: number;
+      toHubId: number;
+      command: string;
+      status: string;
+      result: string;
+      sandboxType: string;
+      requestedAt: string;
+      requestedByUserId: number;
+      requestedByUser: User;
+      startedAt: string;
+      completedAt: string;
+      tasks: Array<Task>;
+    };
+    currentUserHasAccess: boolean | null;
+    currentUserHasSuperAdminAccess: boolean | null;
+    requestAccessFrom: User | null;
+    superAdminsInSandbox: number | null;
+  };
+  personalAccessKey: string;
+};
+
+export type SandboxUsageLimits = {
+  usage: {
+    STANDARD: {
+      used: number;
+      available: number;
+      limit: number;
+    };
+    DEVELOPER: {
+      used: number;
+      available: number;
+      limit: number;
+    };
+  };
+};
