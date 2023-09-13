fa0561e4d9e4f7be411ee3b02cd3f017fda53a82
Allison Kemmerle <akemmerle@hubspot.com>
2023-09-13 14:04:32 -0400

    Add schema endpoints, types, and methods

---
+++ api/schema.ts
@@ -0,0 +1,52 @@
+import fs from 'fs-extra';
+import http from '../http';
+import { FetchSchemasResponse, Schema } from '../types/Schemas';
+
+const SCHEMA_API_PATH = 'crm-object-schemas/v3/schemas';
+
+export function createSchema(
+  accountId: number,
+  filePath: string
+): Promise<Schema> {
+  return http.post(accountId, {
+    url: SCHEMA_API_PATH,
+    body: JSON.parse(fs.readFileSync(filePath, 'utf-8')),
+  });
+}
+
+export async function updateSchema(
+  accountId: number,
+  schemaObjectType: string,
+  filePath: string
+): Promise<Schema> {
+  return http.patch(accountId, {
+    url: `${SCHEMA_API_PATH}/${schemaObjectType}`,
+    body: JSON.parse(fs.readFileSync(filePath, 'utf-8')),
+  });
+}
+
+export async function fetchSchema(
+  accountId: number,
+  schemaObjectType: string
+): Promise<Schema> {
+  return http.get(accountId, {
+    url: `${SCHEMA_API_PATH}/${schemaObjectType}`,
+  });
+}
+
+export async function fetchSchemas(
+  accountId: number
+): Promise<FetchSchemasResponse> {
+  return http.get(accountId, {
+    url: SCHEMA_API_PATH,
+  });
+}
+
+export async function deleteSchema(
+  accountId: number,
+  schemaObjectType: string
+): Promise<void> {
+  return http.delete(accountId, {
+    url: `${SCHEMA_API_PATH}/${schemaObjectType}`,
+  });
+}
---
+++ schema.ts
@@ -0,0 +1,43 @@
+import fs from 'fs-extra';
+import path from 'path';
+import prettier from 'prettier';
+import { getCwd } from './lib/path';
+import { fetchSchemas, fetchSchema } from './api/schema';
+import { Schema } from './types/Schemas';
+
+export function getResolvedPath(dest: string, name: string): string {
+  if (name) return path.resolve(getCwd(), dest || '', `${name}.json`);
+
+  return path.resolve(getCwd(), dest || '');
+}
+
+export function writeSchemaToDisk(schema: Schema, dest: string): void {
+  fs.outputFileSync(
+    getResolvedPath(dest, schema.name),
+    prettier.format(JSON.stringify(schema), {
+      parser: 'json',
+    })
+  );
+}
+
+export async function downloadSchemas(
+  accountId: number,
+  dest: string
+): Promise<void> {
+  const response = await fetchSchemas(accountId);
+
+  if (response.results.length) {
+    response.results.forEach(r => writeSchemaToDisk(r, dest));
+  }
+
+  return;
+}
+
+export async function downloadSchema(
+  accountId: number,
+  schemaObjectType: string,
+  dest: string
+): Promise<void> {
+  const response = await fetchSchema(accountId, schemaObjectType);
+  writeSchemaToDisk(response, dest);
+}
