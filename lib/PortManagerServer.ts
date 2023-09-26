9b660ea09aac39d7f6f04e35f77274428a745f86
Camden Phalen <cphalen@hubspot.com>
2023-09-26 16:51:42 -0400

    add delete endpoint + clean some things up

--- lib/PortManagerServer.ts
+++ lib/PortManagerServer.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import { Server } from 'http';
 
 import { detectPort } from '../utils/detectPort';
 import {
@@ -13,6 +14,7 @@ type PortMap = {
 
 class PortManagerServer {
   app?: Express;
+  server?: Server;
   ports: PortMap;
 
   constructor() {
@@ -28,9 +30,15 @@ class PortManagerServer {
     this.app = express();
     this.app.use(express.json());
     this.setupRoutes();
-    this.app.listen(PORT_MANAGER_SERVER_PORT, () =>
+    this.server = this.app.listen(PORT_MANAGER_SERVER_PORT, () =>
       console.log('Running port manager')
     );
+  }
+
+  close(): void {
+    if (this.server) {
+      this.server.close();
+    }
   }
 
   setupRoutes(): void {
@@ -40,10 +48,17 @@ class PortManagerServer {
 
     this.app.get('/servers/:instanceId', this.getPortByInstanceId);
     this.app.post('/servers', this.assignPortToServer);
+    this.app.delete('/servers/:instanceId', this.deleteServerInstance);
   }
 
   setPort(instanceId: string, port: number) {
     this.ports[instanceId] = port;
+  }
+
+  send404(res: Response, instanceId: string) {
+    res
+      .status(404)
+      .send(`Could not find a server with instanceId ${instanceId}`);
   }
 
   getPortByInstanceId = (req: Request, res: Response): void => {
@@ -53,14 +68,18 @@ class PortManagerServer {
     if (port) {
       res.send({ port });
     } else {
-      res
-        .status(404)
-        .send(`Could not find a server with instanceId ${instanceId}`);
+      this.send404(res, instanceId);
     }
   };
 
   assignPortToServer = async (req: Request, res: Response): Promise<void> => {
     const { instanceId, port } = req.body;
+
+    if (this.ports[instanceId]) {
+      res
+        .status(409)
+        .send('This server instance has already been assigned a port');
+    }
 
     if (port && (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER)) {
       res.status(400).send('Port must be between 1024 and 65535');
@@ -70,6 +89,18 @@ class PortManagerServer {
     this.setPort(instanceId, portToUse);
 
     res.send({ port: portToUse });
+  };
+
+  deleteServerInstance = (req: Request, res: Response): void => {
+    const { instanceId } = req.params;
+    const port = this.ports[instanceId];
+
+    if (port) {
+      delete this.ports[instanceId];
+      res.send(200);
+    } else {
+      this.send404(res, instanceId);
+    }
   };
 }
 
