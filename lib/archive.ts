1f2ebef4e8fee7929c809d38e0ade876c3513c7a
Camden Phalen <cphalen@hubspot.com>
2023-11-16 13:43:13 -0500

    Add log callbacks to archive

--- lib/archive.ts
+++ lib/archive.ts
@@ -6,8 +6,9 @@ import __extract from 'extract-zip';
 
 import { throwFileSystemError } from '../errors/fileSystemErrors';
 import { throwErrorWithMessage } from '../errors/standardErrors';
-import { debug } from '../utils/logger';
+import { debug, makeTypedLogger } from '../utils/logger';
 import { BaseError } from '../types/Error';
+import { LogCallbacksArg } from '../types/LogCallbacks';
 
 const extract = promisify(__extract);
 
@@ -18,11 +19,18 @@ type ZipData = {
   tmpDir: string;
 };
 
-async function extractZip(name: string, zip: Buffer): Promise<ZipData> {
+const archiveCallbackKeys = ['init', 'copy'];
+
+async function extractZip(
+  name: string,
+  zip: Buffer,
+  logCallbacks?: LogCallbacksArg<typeof archiveCallbackKeys>
+): Promise<ZipData> {
+  const logger = makeTypedLogger<typeof archiveCallbackKeys>(logCallbacks);
   const result: ZipData = { extractDir: '', tmpDir: '' };
 
   const TMP_FOLDER_PREFIX = `hubspot-temp-${name}-`;
-  debug(`${i18nKey}.extractZip.init`);
+  logger('init', `${i18nKey}.extractZip.init`);
 
   // Write zip to disk
   let tmpZipPath = '';
@@ -72,10 +80,12 @@ type CopySourceToDestOptions = {
 async function copySourceToDest(
   src: string,
   dest: string,
-  { sourceDir, includesRootDir = true }: CopySourceToDestOptions = {}
+  { sourceDir, includesRootDir = true }: CopySourceToDestOptions = {},
+  logCallbacks?: LogCallbacksArg<typeof archiveCallbackKeys>
 ): Promise<boolean> {
   try {
-    debug(`${i18nKey}.copySourceToDest.init`);
+    const logger = makeTypedLogger<typeof archiveCallbackKeys>(logCallbacks);
+    logger('copy', `${i18nKey}.copySourceToDest.init`);
     const srcDirPath = [src];
 
     if (includesRootDir) {
@@ -123,18 +133,24 @@ export async function extractZipArchive(
   zip: Buffer,
   name: string,
   dest: string,
-  { sourceDir, includesRootDir }: CopySourceToDestOptions = {}
+  { sourceDir, includesRootDir }: CopySourceToDestOptions = {},
+  logCallbacks?: LogCallbacksArg<typeof archiveCallbackKeys>
 ): Promise<boolean> {
   let success = false;
 
   if (zip) {
-    const { extractDir, tmpDir } = await extractZip(name, zip);
+    const { extractDir, tmpDir } = await extractZip(name, zip, logCallbacks);
 
     if (extractDir !== null) {
-      success = await copySourceToDest(extractDir, dest, {
-        sourceDir,
-        includesRootDir,
-      });
+      success = await copySourceToDest(
+        extractDir,
+        dest,
+        {
+          sourceDir,
+          includesRootDir,
+        },
+        logCallbacks
+      );
     }
 
     cleanupTempDir(tmpDir);
