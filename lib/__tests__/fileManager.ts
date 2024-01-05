c33e2689eb857bbded39a7b39dd2a70d9ada73f6
Camden Phalen <cphalen@hubspot.com>
2024-01-05 15:23:41 -0500

    Filemanager tests

---
+++ lib/__tests__/fileManager.ts
@@ -0,0 +1,43 @@
+import { uploadFolder } from '../fileManager';
+import { uploadFile } from '../../api/fileManager';
+import { walk } from '../fs';
+import { createIgnoreFilter } from '../ignoreRules';
+
+jest.mock('../lib/walk');
+jest.mock('../api/fileManager');
+jest.mock('../ignoreRules');
+
+describe('uploadFolder', () => {
+  describe('uploadFolder()', () => {
+    it('uploads files in the folder', async () => {
+      const files = [
+        'folder/document.pdf',
+        'folder/images/image.png',
+        'folder/images/image.jpg',
+        'folder/video/video.mp4',
+      ];
+
+      const mockedWalk = jest.mocked(walk);
+      const mockedUploadFile = jest.mocked(uploadFile);
+      const mockedCreateIgnoreFilter = jest.mocked(createIgnoreFilter);
+
+      mockedWalk.mockResolvedValue(files);
+      mockedUploadFile.mockImplementation(() =>
+        Promise.resolve({ objects: [] })
+      );
+      mockedCreateIgnoreFilter.mockImplementation(() => () => true);
+
+      const accountId = 123;
+      const src = 'folder';
+      const dest = 'folder';
+
+      await uploadFolder(accountId, src, dest);
+
+      expect(uploadFile).toReturnTimes(4);
+
+      files.forEach((file, index) => {
+        expect(uploadFile).nthCalledWith(index + 1, accountId, file, file);
+      });
+    });
+  });
+});
