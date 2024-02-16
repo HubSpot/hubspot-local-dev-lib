af431d93a56ed31e87e32e37f25312245f4f164a
Jessica Sines <jsines@hubspot.com>
2024-02-16 15:29:44 -0500

    New lib versions of the files got missed from git add

---
+++ lib/notify.ts
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import moment from 'moment';
+import debounce from 'debounce';
+
+import { throwErrorWithMessage } from '../errors/standardErrors';
+import { BaseError } from '../types/Error';
+
+const i18nKey = 'utils.notify';
+
+const notifyQueue: Array<string> = [];
+const notifyPromises: Array<Promise<void>> = [];
+const debouncedWaitForActionsToCompleteAndWriteQueueToFile = debounce(
+  waitForActionsToCompleteAndWriteQueueToFile,
+  500
+);
+
+// Collects actions that have been taken on files and the corresponding Promise
+// for the remote action that is in-process
+export function triggerNotify(
+  filePathToNotify: string | undefined,
+  actionType: string,
+  filePath: string,
+  actionPromise: Promise<void>
+): void {
+  if (filePathToNotify) {
+    notifyQueue.push(`${moment().toISOString()} ${actionType}: ${filePath}\n`);
+    notifyPromises.push(actionPromise);
+    debouncedWaitForActionsToCompleteAndWriteQueueToFile(filePathToNotify);
+  }
+}
+
+// Clears both the notifyQueue and notifyPromises array, generates the output
+// string that will be eventually logged, and waits for all promises currently
+// in the notifyPromises array to resolve before logging the output
+function waitForActionsToCompleteAndWriteQueueToFile(
+  filePathToNotify: string
+): void {
+  const actionOutput = notifyQueue.splice(0, notifyQueue.length).join('');
+  const allNotifyPromisesResolution = Promise.all(
+    notifyPromises.splice(0, notifyPromises.length)
+  );
+
+  allNotifyPromisesResolution.then(() =>
+    notifyFilePath(filePathToNotify, actionOutput)
+  );
+}
+
+// Logs output to the "notify" file
+function notifyFilePath(filePathToNotify: string, outputToWrite: string): void {
+  if (filePathToNotify) {
+    try {
+      fs.appendFileSync(filePathToNotify, outputToWrite);
+    } catch (e) {
+      throwErrorWithMessage(
+        `${i18nKey}.errors.filePath`,
+        { filePath: filePathToNotify },
+        e as BaseError
+      );
+    }
+  }
+}
