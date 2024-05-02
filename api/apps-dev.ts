36f91ece38cfef1823b3f55f359d88adc6304338
Camden Phalen <cphalen@hubspot.com>
2024-05-02 15:09:50 -0400

    Add types for fetchPublicAppsForPortal

--- api/apps-dev.ts
+++ api/apps-dev.ts
@@ -1,9 +1,16 @@
 import http from '../http';
+import { PublicApp } from '../types/Apps';
 
 const APPS_DEV_API_PATH = 'apps-dev/external/public/v3';
 
-export function fetchPublicAppsForPortal(portalId: number) {
-  return http.get(portalId, {
+type FetchPublicAppsForPortalResponse = {
+  results: Array<PublicApp>;
+};
+
+export function fetchPublicAppsForPortal(
+  portalId: number
+): Promise<FetchPublicAppsForPortalResponse> {
+  return http.get<FetchPublicAppsForPortalResponse>(portalId, {
     url: `${APPS_DEV_API_PATH}/full/portal`,
   });
 }
--- types/Apps.ts
+++ types/Apps.ts
@@ -6,3 +6,38 @@ export type PublicAppInstallationData = {
     name: string;
   }>;
 };
+
+export type PublicApp = {
+  id: number;
+  name: string;
+  description: string;
+  portalId: number;
+  updatedAt: number;
+  createdAt: number;
+  clientId: string;
+  iconUrl: string;
+  archived: boolean;
+  ownerId: number;
+  isUserLevel: boolean;
+  isBusinessUnitEnabled: boolean;
+  isFeatured: boolean;
+  isInternal: boolean;
+  documentationUrl: string | null;
+  supportUrl: string | null;
+  supportEmail: string | null;
+  supportPhone: string | null;
+  extensionIconUrl: string | null;
+  isAdvancedScopesSettingEnabled: boolean;
+  publicApplicationInstallCounts: {
+    uniquePortalInstallCount: number;
+    uniqueUserInstallCount: number;
+    uniqueBusinessUnitInstallCount: number;
+  };
+  redirectUrls: Array<string>;
+  scopeGroupIds: Array<number>;
+  additionalScopeGroupIds: Array<number>;
+  optionalScopeGroupIds: Array<number>;
+  projectId: number | null;
+  sourceId: string | null;
+  allowedExternalUrls: Array<string>;
+};
