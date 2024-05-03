f860fa299929c3b1f35fe7d4128a599447df0049
Allison Kemmerle <akemmerle@hubspot.com>
2024-05-03 16:14:45 -0400

    Revise API path & remove test data

--- api/appsDev.ts
+++ api/appsDev.ts
@@ -1,41 +1,12 @@
 import http from '../http';
 import { FetchAppsResponse } from '../types/App';
 
-const APPS_API_PATH = 'apps-dev/public/v3';
+const APPS_API_PATH = 'apps-dev/external/public/v3';
 
 export async function fetchPublicApps(
   accountId: number
 ): Promise<FetchAppsResponse> {
   return http.get(accountId, {
-    url: `${APPS_API_PATH}/portal`,
+    url: `${APPS_API_PATH}/full/portal`,
   });
 }
-
-// TEST DATA
-// export async function fetchPublicApps(accountId: number) {
-//   return Promise.resolve([
-//     {
-//       id: 1000000,
-//       name: 'Getting Started App',
-//       description:
-//         'An example project of how to build a Public App with Developer Projects',
-//       portalId: 123456789,
-//       updatedAt: 1713382701719,
-//       createdAt: 1713380475769,
-//       clientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-//       iconUrl: null,
-//       archived: false,
-//       ownerId: 7654321,
-//       isUserLevel: false,
-//       isBusinessUnitEnabled: false,
-//       isFeatured: false,
-//       isInternal: false,
-//       documentationUrl: 'https://example.com/docs',
-//       supportUrl: 'https://example.com/support',
-//       supportEmail: 'support@example.com',
-//       supportPhone: '+18005555555',
-//       extensionIconUrl: null,
-//       isAdvancedScopesSettingEnabled: true,
-//     },
-//   ]);
-// }
