1eda3d0c0a9dcd4efcbbe6ec8fa5c8d2c04ecf52
Joe Yeager <jyeager@hubspot.com>
2024-06-10 10:33:48 -0700

    chore: Expand test coverage

--- lib/__tests__/oauth.ts
+++ lib/__tests__/oauth.ts
@@ -1,12 +1,80 @@
+import { addOauthToAccountConfig, getOauthManager } from '../oauth';
+
+jest.mock('../../utils/getAccountIdentifier');
+jest.mock('../../config');
+jest.mock('../logger');
+
+import { updateAccountConfig, writeConfig } from '../../config';
+import OAuth2Manager from '../../models/OAuth2Manager';
+import { FlatAccountFields_NEW } from '../../types/Accounts';
+import { ENVIRONMENTS } from '../../constants/environments';
+import { AUTH_METHODS } from '../../constants/auth';
+import { logger } from '../logger';
+
+const writeConfigMock = writeConfig as jest.MockedFunction<typeof writeConfig>;
+const OAuth2ManagerFromConfigMock = jest.spyOn(OAuth2Manager, 'fromConfig');
+
 describe('lib/oauth', () => {
+  const accountId = 123;
+  const accountConfig: FlatAccountFields_NEW = {
+    accountId,
+    env: ENVIRONMENTS.QA,
+    clientId: 'my-client-id',
+    clientSecret: "shhhh, it's a secret",
+    scopes: [],
+    apiKey: '',
+    personalAccessKey: '',
+  };
+  const account = {
+    name: 'my-account',
+  };
   describe('getOauthManager', () => {
-    it('should have a test', () => {
-      expect(false).toEqual(true);
+    it('should create a OAuth2Manager for accounts that are not cached', () => {
+      getOauthManager(accountId, accountConfig);
+      expect(OAuth2ManagerFromConfigMock).toHaveBeenCalledTimes(1);
+      expect(OAuth2ManagerFromConfigMock).toHaveBeenCalledWith(
+        accountConfig,
+        expect.any(Function)
+      );
+    });
+
+    it('should use the cached OAuth2Manager if it exists', () => {
+      getOauthManager(accountId, accountConfig);
+      expect(OAuth2ManagerFromConfigMock).not.toHaveBeenCalled();
     });
   });
+
   describe('addOauthToAccountConfig', () => {
-    it('should have a test', () => {
-      expect(false).toEqual(true);
+    it('should update the config', () => {
+      addOauthToAccountConfig(new OAuth2Manager(account));
+      expect(updateAccountConfig).toHaveBeenCalledTimes(1);
+      expect(updateAccountConfig).toHaveBeenCalledWith({
+        ...account,
+        authType: AUTH_METHODS.oauth.value,
+      });
+    });
+
+    it('should write the updated config', () => {
+      addOauthToAccountConfig(new OAuth2Manager(account));
+      expect(writeConfig).toHaveBeenCalledTimes(1);
+    });
+
+    it('should log messages letting the user know the status of the operation', () => {
+      addOauthToAccountConfig(new OAuth2Manager(account));
+      expect(logger.log).toHaveBeenCalledTimes(1);
+      expect(logger.log).toHaveBeenCalledWith('Updating configuration');
+      expect(logger.success).toHaveBeenCalledTimes(1);
+      expect(logger.success).toHaveBeenCalledWith('Configuration updated');
+    });
+
+    it('should an error when an error is encountered', () => {
+      const errorMessage = 'OH NO';
+      writeConfigMock.mockImplementationOnce(() => {
+        throw new Error(errorMessage);
+      });
+      expect(() => {
+        addOauthToAccountConfig(new OAuth2Manager(account));
+      }).toThrow(errorMessage);
     });
   });
 });
