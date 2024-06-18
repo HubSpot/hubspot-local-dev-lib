0bd1e7af36ba742c9ee87572ec0aea2075a851cf
Joe Yeager <jyeager@hubspot.com>
2024-06-18 15:35:34 -0700

    Clean up, some more thoughts.

--- http/index.ts
+++ http/index.ts
@@ -29,28 +29,31 @@ export class HubSpotHttpError<T = unknown, D = unknown> extends Error {
   public statusText?: string;
   public data?: T;
 
+  // Add this in so any pre-existing checks for error.isAxiosError continue to
+  // function until they can be ported over to the new error check
+  public isAxiosError = true;
+
   constructor(message?: string, options?: ErrorOptions) {
     super(message, options);
     this.name = 'HubSpotHttpError';
 
     if (options && isAxiosError(options.cause)) {
+      // Add these for backwards compatibility until we have updated all the checks
+      // in the CLI for the Axios implementation details and then we can remove these or
+      // keep whatever we find useful in custom fields on this object
+      const { response, request, config, code } = options.cause;
+      this.response = response;
+      this.request = request;
+      this.config = config;
+
       // Add any custom fields we feel are necessary or make our
       // collective lives easier
-      this.code = options.cause.code;
-      if (options.cause.response) {
-        const { response } = options.cause;
+      this.code = code;
+      if (response) {
         this.status = response.status;
         this.statusText = response.statusText;
         this.data = response.data;
       }
-
-      // Add these for backwards compatibility until we have updated all the checks
-      // in the CLI for the Axios implementation details and then we can remove these or
-      // keep whatever we find useful in custom fields on this object
-      const { response, request, config } = options.cause;
-      this.response = response;
-      this.request = request;
-      this.config = config;
     }
   }
 }
