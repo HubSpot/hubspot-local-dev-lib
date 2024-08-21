b8118153a9ca502fb23bfb0e6cbecae3e855a897
Joe Yeager <jyeager@hubspot.com>
2024-08-21 09:41:38 -0700

    Rolback changes to api/functions while we wait on backend

--- api/functions.ts
+++ api/functions.ts
@@ -2,19 +2,11 @@ import http from '../http';
 import { QueryParams } from '../types/Http';
 import { GetBuildStatusResponse, GetRoutesResponse } from '../types/Functions';
 
-const APP_FUNCTION_API_PATH = '/app-functions';
-const CMS_FUNCTION_API_PATH = 'cms/v3/functions';
-const DEVELOPER_API_PATH = 'developer/platform/logging';
+const FUNCTION_API_PATH = 'cms/v3/functions';
 
 export async function getRoutes(accountId: number): Promise<GetRoutesResponse> {
   return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/routes`,
-  });
-}
-
-export async function getAppFunctions(accountId: number, appId: number) {
-  return http.get(accountId, {
-    url: `${APP_FUNCTION_API_PATH}/${appId}/functions`,
+    url: `${FUNCTION_API_PATH}/routes`,
   });
 }
 
@@ -26,49 +18,16 @@ export async function getFunctionLogs(
   const { limit = 5 } = params;
 
   return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/results/by-route/${encodeURIComponent(route)}`,
+    url: `${FUNCTION_API_PATH}/results/by-route/${encodeURIComponent(route)}`,
     params: { ...params, limit },
   });
 }
 
 export async function getLatestFunctionLog(accountId: number, route: string) {
   return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/results/by-route/${encodeURIComponent(
+    url: `${FUNCTION_API_PATH}/results/by-route/${encodeURIComponent(
       route
     )}/latest`,
-  });
-}
-
-export async function getPublicFunctionLogs(
-  accountId: number,
-  functionName: string,
-  projectName: string,
-  appPath: string,
-  params: QueryParams = {}
-) {
-  const { limit = 5 } = params;
-  return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/app-function/logs/project/${projectName}/function/${functionName}`,
-    params: { appPath, ...params, limit },
-  });
-}
-
-export async function getLatestPublicFunctionLogs(
-  accountId: number,
-  functionName: string,
-  projectName: string,
-  appPath: string
-) {
-  return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/app-function/logs/project/${projectName}/function/${functionName}/latest`,
-    params: { appPath },
-  });
-}
-
-export async function getAppLogs(accountId: number, appId: string) {
-  return http.post(accountId, {
-    url: `${DEVELOPER_API_PATH}/search`,
-    data: { appId },
   });
 }
 
@@ -77,7 +36,7 @@ export async function buildPackage(
   folderPath: string
 ): Promise<string> {
   return http.post(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/build/async`,
+    url: `${FUNCTION_API_PATH}/build/async`,
     headers: {
       Accept: 'text/plain',
     },
@@ -92,6 +51,6 @@ export async function getBuildStatus(
   buildId: number
 ): Promise<GetBuildStatusResponse> {
   return http.get(accountId, {
-    url: `${CMS_FUNCTION_API_PATH}/build/${buildId}/poll`,
+    url: `${FUNCTION_API_PATH}/build/${buildId}/poll`,
   });
 }
