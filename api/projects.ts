77b753a1f91c6307b6555f4691d7d1e965e1e36f
Allison Kemmerle <akemmerle@hubspot.com>
2024-08-29 12:35:39 -0400

    Revert two files

--- api/projects.ts
+++ api/projects.ts
@@ -76,9 +76,7 @@ export async function fetchProject(
   projectName: string
 ): Promise<Project> {
   return http.get(accountId, {
-    url: `${DEVELOPER_PROJECTS_API_PATH}/by-name/${encodeURIComponent(
-      projectName
-    )}`,
+    url: `${DEVELOPER_PROJECTS_API_PATH}/by-name/${encodeURIComponent(projectName)}`,
   });
 }
 
@@ -200,9 +198,7 @@ export async function fetchProjectSettings(
   projectName: string
 ): Promise<ProjectSettings> {
   return http.get(accountId, {
-    url: `${DEVELOPER_PROJECTS_API_PATH}/${encodeURIComponent(
-      projectName
-    )}/settings`,
+    url: `${DEVELOPER_PROJECTS_API_PATH}/${encodeURIComponent(projectName)}/settings`,
   });
 }
 
