67d276f32a7efc5f427f08e36dd7acb66c4668af
Branden Rodgers <brodgers@hubspot.com>
2024-09-18 12:43:23 -0400

    more future-proof solution

--- lib/trackUsage.ts
+++ lib/trackUsage.ts
@@ -5,7 +5,6 @@ import http from '../http';
 import { getAccountConfig, getEnv } from '../config';
 import { FILE_MAPPER_API_PATH } from '../api/fileMapper';
 import { i18n } from '../utils/lang';
-import { HUBSPOT_ACCOUNT_TYPES } from '../constants/config';
 
 const i18nKey = 'lib.trackUsage';
 
@@ -43,17 +42,18 @@ export async function trackUsage(
 
   const accountConfig = accountId && getAccountConfig(accountId);
 
-  if (
-    accountConfig &&
-    accountConfig.authType === 'personalaccesskey' &&
-    accountConfig.accountType !== HUBSPOT_ACCOUNT_TYPES.APP_DEVELOPER
-  ) {
+  if (accountConfig && accountConfig.authType === 'personalaccesskey') {
     logger.debug(i18n(`${i18nKey}.sendingEventAuthenticated`));
-    return http.post(accountId, {
-      url: `${path}/authenticated`,
-      data: usageEvent,
-      resolveWithFullResponse: true,
-    });
+    try {
+      const result: void = await http.post(accountId, {
+        url: `${path}/authenticated`,
+        data: usageEvent,
+        resolveWithFullResponse: true,
+      });
+      return result;
+    } catch (e) {
+      logger.debug(i18n(`${i18nKey}.retryingEventUnauthenticated`));
+    }
   }
 
   const env = getEnv(accountId);
