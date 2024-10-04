69b4bee96e0e1fb612ed8f7ffb8adf51a829700c
Joe Yeager <jyeager@hubspot.com>
2024-10-04 09:57:17 -0700

    chore: Add changelog script

---
+++ scripts/changelog.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+TAG=$(git describe --tags --abbrev=0)
+printf "\e[1;36mGetting changelog for $(basename -s .git `git config --get remote.origin.url`) since $TAG\e[1;0m\n"
+git --no-pager log $TAG..main --pretty=format:'- %Cgreen %s %Creset–%Cblue @%al %Creset' --no-merges --source
+printf "\n"
