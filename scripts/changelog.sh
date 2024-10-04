b71ad4fbf71e2650deb99dbfc50192f5d699dc16
Joe Yeager <jyeager@hubspot.com>
2024-10-04 09:59:54 -0700

    Use origin/main

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -2,5 +2,5 @@
 
 TAG=$(git describe --tags --abbrev=0)
 printf "\e[1;36mGetting changelog for $(basename -s .git `git config --get remote.origin.url`) since $TAG\e[1;0m\n"
-git --no-pager log $TAG..main --pretty=format:'- %Cgreen %s %Creset–%Cblue @%al %Creset' --no-merges --source
+git --no-pager log $TAG..origin/main --pretty=format:'- %Cgreen %s %Creset–%Cblue @%al %Creset' --no-merges --source
 printf "\n"
