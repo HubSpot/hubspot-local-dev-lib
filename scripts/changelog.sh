785e6d9298374c6451f904eb9693b3d7ed31e3cb
Joe Yeager <jyeager@hubspot.com>
2024-10-04 10:06:11 -0700

    improvements

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Pull the repo and pipe only stdout to /dev/null
+git pull 1> /dev/null
 
+# Get the most recent tag
 TAG=$(git describe --tags --abbrev=0)
+
 printf "\e[1;36mGetting changelog for $(basename -s .git `git config --get remote.origin.url`) since $TAG\e[1;0m\n"
+
+# Get the commit log diff between the most recent tag and origin/main
 git --no-pager log $TAG..origin/main --pretty=format:'- %Cgreen %s %Creset–%Cblue @%al %Creset' --no-merges --source
 printf "\n"
