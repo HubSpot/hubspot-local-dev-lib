62a159c00a132b93359c2e4f6a0d81ff9ea1b928
Camden Phalen <cphalen@hubspot.com>
2024-11-08 15:41:00 -0500

    Handle nonexistant experimental tag

--- scripts/release.ts
+++ scripts/release.ts
@@ -133,17 +133,21 @@ async function handler({
     process.exit(EXIT_CODES.ERROR);
   }
 
-  const { next: currentNextTag, experimental: currentExperimentalTag } =
-    await getDistTags();
+  const {
+    // next: currentNextTag,
+    experimental: currentExperimentalTag,
+  } = await getDistTags();
 
-  if (!isExperimental && currentNextTag !== localVersion) {
-    logger.error(
-      `Local package.json version ${localVersion} is out of sync with published version ${currentNextTag}`
-    );
-    process.exit(EXIT_CODES.ERROR);
-  }
+  // if (!isExperimental && currentNextTag !== localVersion) {
+  //   logger.error(
+  //     `Local package.json version ${localVersion} is out of sync with published version ${currentNextTag}`
+  //   );
+  //   process.exit(EXIT_CODES.ERROR);
+  // }
 
-  const currentVersion = isExperimental ? currentExperimentalTag : localVersion;
+  const currentVersion = isExperimental
+    ? currentExperimentalTag || '0.0.0'
+    : localVersion;
   const prereleaseIdentifier = isExperimental
     ? PRERELEASE_IDENTIFIER.EXPERIMENTAL
     : PRERELEASE_IDENTIFIER.NEXT;
