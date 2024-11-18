ccc87bf71edc782242afdad45b88244661dda334
Camden Phalen <cphalen@hubspot.com>
2024-11-18 14:25:27 -0500

    Remove experimental-temp tag

--- scripts/release.ts
+++ scripts/release.ts
@@ -23,9 +23,6 @@ const TAG = {
   EXPERIMENTAL: 'experimental',
 } as const;
 
-// remove
-const EXPERIMENTAL_TEMP = 'experimental-temp';
-
 const INCREMENT = {
   PATCH: 'patch',
   MINOR: 'minor',
@@ -131,13 +128,13 @@ async function updateNextTag(
   isDryRun: boolean
 ): Promise<void> {
   logger.log();
-  logger.log(`Updating ${EXPERIMENTAL_TEMP} tag...`);
+  logger.log(`Updating ${TAG.NEXT} tag...`);
 
   const commandArgs = [
     'dist-tag',
     'add',
     `${packageName}@${newVersion}`,
-    EXPERIMENTAL_TEMP,
+    TAG.NEXT,
     '--registry',
     REGISTRY,
     '--otp',
@@ -156,7 +153,7 @@ async function updateNextTag(
         if (code !== EXIT_CODES.SUCCESS) {
           reject();
         } else {
-          logger.success(`${EXPERIMENTAL_TEMP} tag updated successfully`);
+          logger.success(`${TAG.NEXT} tag updated successfully`);
           resolve();
         }
       });
