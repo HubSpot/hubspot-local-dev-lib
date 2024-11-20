3b735a1ab0f3c73e5aab84bb817a885818728a3d
Camden Phalen <cphalen@hubspot.com>
2024-11-20 14:32:21 -0500

    chore: Update config mode to Mode type

--- types/Config.ts
+++ types/Config.ts
@@ -1,12 +1,13 @@
 import { ENVIRONMENTS } from '../constants/environments';
 import { CLIAccount_NEW, CLIAccount_DEPRECATED } from './Accounts';
+import { Mode } from './Files';
 import { ValueOf } from './Utils';
 
 export interface CLIConfig_NEW {
   accounts: Array<CLIAccount_NEW>;
   allowUsageTracking?: boolean;
   defaultAccount?: string | number;
-  defaultMode?: string;
+  defaultMode?: Mode;
   httpTimeout?: number;
   env?: Environment;
   httpUseLocalhost?: boolean;
@@ -16,7 +17,7 @@ export interface CLIConfig_DEPRECATED {
   portals: Array<CLIAccount_DEPRECATED>;
   allowUsageTracking?: boolean;
   defaultPortal?: string | number;
-  defaultMode?: string;
+  defaultMode?: Mode;
   httpTimeout?: number;
   env?: Environment;
   httpUseLocalhost?: boolean;
