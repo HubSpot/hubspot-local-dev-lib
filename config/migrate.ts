7eb31fc1bf9afaa1b959c0fd2befdd74d94f1a4f
Camden Phalen <cphalen@hubspot.com>
2025-04-04 15:54:09 -0400

    Updateconfig migrate for revamped config

--- config/migrate.ts
+++ config/migrate.ts
@@ -1,218 +1,119 @@
-import * as config_DEPRECATED from './config_DEPRECATED';
-import { CLIConfiguration } from './CLIConfiguration';
+import fs from 'fs';
+
+import { HubSpotConfig } from '../types/Config';
+import { createEmptyConfigFile } from './index';
 import {
-  CLIConfig,
-  CLIConfig_DEPRECATED,
-  CLIConfig_NEW,
-  Environment,
-} from '../types/Config';
-import { CmsPublishMode } from '../types/Files';
-import {
-  writeConfig,
-  createEmptyConfigFile,
-  loadConfig,
-  deleteEmptyConfigFile,
-} from './index';
-import {
-  getConfigFilePath,
-  configFileExists as newConfigFileExists,
-} from './configFile';
-import {
-  GLOBAL_CONFIG_PATH,
   DEFAULT_CMS_PUBLISH_MODE,
   HTTP_TIMEOUT,
   ENV,
   HTTP_USE_LOCALHOST,
   ALLOW_USAGE_TRACKING,
   DEFAULT_ACCOUNT,
-  DEFAULT_PORTAL,
 } from '../constants/config';
-import { i18n } from '../utils/lang';
+import {
+  getGlobalConfigFilePath,
+  parseConfig,
+  readConfigFile,
+  writeConfigFile,
+} from './utils';
+import { ValueOf } from '../types/Utils';
 
-const i18nKey = 'config.migrate';
+function getConfigAtPath(path: string): HubSpotConfig {
+  const configFileSource = readConfigFile(path);
 
-export function getDeprecatedConfig(
-  configPath?: string
-): CLIConfig_DEPRECATED | null {
-  return config_DEPRECATED.loadConfig(configPath);
+  return parseConfig(configFileSource);
 }
 
-export function getGlobalConfig(): CLIConfig_NEW | null {
-  if (CLIConfiguration.isActive()) {
-    return CLIConfiguration.config;
-  }
-  return null;
+export function migrateConfigAtPath(path: string): void {
+  createEmptyConfigFile(true);
+  const configToMigrate = getConfigAtPath(path);
+  writeConfigFile(configToMigrate, getGlobalConfigFilePath());
+  fs.unlinkSync(path);
 }
 
-export function configFileExists(
-  useHiddenConfig = false,
-  configPath?: string
-): boolean {
-  return useHiddenConfig
-    ? newConfigFileExists()
-    : Boolean(config_DEPRECATED.getConfigPath(configPath));
-}
-
-export function getConfigPath(
-  configPath?: string,
-  useHiddenConfig = false
-): string | null {
-  if (useHiddenConfig) {
-    return getConfigFilePath();
-  }
-  return config_DEPRECATED.getConfigPath(configPath);
-}
-
-function writeGlobalConfigFile(
-  updatedConfig: CLIConfig_NEW,
-  isMigrating = false
-): void {
-  const updatedConfigJson = JSON.stringify(updatedConfig);
-  if (isMigrating) {
-    createEmptyConfigFile({}, true);
-  }
-  loadConfig('');
-
-  try {
-    writeConfig({ source: updatedConfigJson });
-    config_DEPRECATED.deleteConfigFile();
-  } catch (error) {
-    deleteEmptyConfigFile();
-    throw new Error(
-      i18n(`${i18nKey}.errors.writeConfig`, { configPath: GLOBAL_CONFIG_PATH }),
-      { cause: error }
-    );
-  }
-}
-
-export function migrateConfig(
-  deprecatedConfig: CLIConfig_DEPRECATED | null
-): void {
-  if (!deprecatedConfig) {
-    throw new Error(i18n(`${i18nKey}.errors.noDeprecatedConfig`));
-  }
-  const { defaultPortal, portals, ...rest } = deprecatedConfig;
-  const updatedConfig = {
-    ...rest,
-    defaultAccount: defaultPortal,
-    accounts: portals
-      .filter(({ portalId }) => portalId !== undefined)
-      .map(({ portalId, ...rest }) => ({
-        ...rest,
-        accountId: portalId!,
-      })),
-  };
-  writeGlobalConfigFile(updatedConfig, true);
-}
-
-type ConflictValue = boolean | string | number | CmsPublishMode | Environment;
 export type ConflictProperty = {
-  property: keyof CLIConfig_NEW;
-  oldValue: ConflictValue;
-  newValue: ConflictValue;
+  property: keyof HubSpotConfig;
+  oldValue: ValueOf<HubSpotConfig>;
+  newValue: ValueOf<HubSpotConfig>;
 };
 
 export function mergeConfigProperties(
-  globalConfig: CLIConfig_NEW,
-  deprecatedConfig: CLIConfig_DEPRECATED,
+  toConfig: HubSpotConfig,
+  fromConfig: HubSpotConfig,
   force?: boolean
 ): {
-  initialConfig: CLIConfig_NEW;
+  configWithMergedProperties: HubSpotConfig;
   conflicts: Array<ConflictProperty>;
 } {
-  const propertiesToCheck: Array<keyof Partial<CLIConfig>> = [
-    DEFAULT_CMS_PUBLISH_MODE,
-    HTTP_TIMEOUT,
-    ENV,
-    HTTP_USE_LOCALHOST,
-    ALLOW_USAGE_TRACKING,
-  ];
   const conflicts: Array<ConflictProperty> = [];
 
-  propertiesToCheck.forEach(prop => {
-    if (prop in globalConfig && prop in deprecatedConfig) {
-      if (force || globalConfig[prop] === deprecatedConfig[prop]) {
-        // @ts-expect-error Cannot reconcile CLIConfig_NEW and CLIConfig_DEPRECATED types
-        globalConfig[prop] = deprecatedConfig[prop];
-      } else {
+  if (force) {
+    toConfig.defaultCmsPublishMode = fromConfig.defaultCmsPublishMode;
+    toConfig.httpTimeout = fromConfig.httpTimeout;
+    toConfig.env = fromConfig.env;
+    toConfig.httpUseLocalhost = fromConfig.httpUseLocalhost;
+    toConfig.allowUsageTracking = fromConfig.allowUsageTracking;
+    toConfig.defaultAccount = fromConfig.defaultAccount;
+  } else {
+    const propertiesToCheck = [
+      DEFAULT_CMS_PUBLISH_MODE,
+      HTTP_TIMEOUT,
+      ENV,
+      HTTP_USE_LOCALHOST,
+      ALLOW_USAGE_TRACKING,
+      DEFAULT_ACCOUNT,
+    ] as const;
+
+    propertiesToCheck.forEach(prop => {
+      if (toConfig[prop] !== fromConfig[prop]) {
         conflicts.push({
           property: prop,
-          oldValue: deprecatedConfig[prop]!,
-          newValue: globalConfig[prop]!,
+          oldValue: fromConfig[prop],
+          newValue: toConfig[prop],
         });
       }
+    });
+  }
+
+  return { configWithMergedProperties: toConfig, conflicts };
+}
+
+function buildConfigWithMergedAccounts(
+  toConfig: HubSpotConfig,
+  fromConfig: HubSpotConfig
+): {
+  configWithMergedAccounts: HubSpotConfig;
+  skippedAccountIds: Array<number>;
+} {
+  const existingAccountIds = toConfig.accounts.map(
+    ({ accountId }) => accountId
+  );
+  const skippedAccountIds: Array<number> = [];
+
+  fromConfig.accounts.forEach(account => {
+    if (existingAccountIds.includes(account.accountId)) {
+      skippedAccountIds.push(account.accountId);
+    } else {
+      toConfig.accounts.push(account);
     }
   });
 
-  if (
-    DEFAULT_ACCOUNT in globalConfig &&
-    DEFAULT_PORTAL in deprecatedConfig &&
-    globalConfig.defaultAccount !== deprecatedConfig.defaultPortal
-  ) {
-    if (force) {
-      globalConfig.defaultAccount = deprecatedConfig.defaultPortal;
-    } else {
-      conflicts.push({
-        property: DEFAULT_ACCOUNT,
-        oldValue: deprecatedConfig.defaultPortal!,
-        newValue: globalConfig.defaultAccount!,
-      });
-    }
-  } else if (DEFAULT_PORTAL in deprecatedConfig) {
-    globalConfig.defaultAccount = deprecatedConfig.defaultPortal;
-  }
-
-  return { initialConfig: globalConfig, conflicts };
-}
-
-function mergeAccounts(
-  globalConfig: CLIConfig_NEW,
-  deprecatedConfig: CLIConfig_DEPRECATED
-): {
-  finalConfig: CLIConfig_NEW;
-  skippedAccountIds: Array<string | number>;
-} {
-  let existingAccountIds: Array<string | number> = [];
-  const skippedAccountIds: Array<string | number> = [];
-
-  if (globalConfig.accounts && deprecatedConfig.portals) {
-    existingAccountIds = globalConfig.accounts.map(
-      account => account.accountId
-    );
-
-    const newAccounts = deprecatedConfig.portals
-      .filter(portal => {
-        const isExisting = existingAccountIds.includes(portal.portalId!);
-        if (isExisting) {
-          skippedAccountIds.push(portal.portalId!);
-        }
-        return !isExisting;
-      })
-      .map(({ portalId, ...rest }) => ({
-        ...rest,
-        accountId: portalId!,
-      }));
-
-    if (newAccounts.length > 0) {
-      globalConfig.accounts.push(...newAccounts);
-    }
-  }
-
   return {
-    finalConfig: globalConfig,
+    configWithMergedAccounts: toConfig,
     skippedAccountIds,
   };
 }
 
-export function mergeExistingConfigs(
-  globalConfig: CLIConfig_NEW,
-  deprecatedConfig: CLIConfig_DEPRECATED
-): { finalConfig: CLIConfig_NEW; skippedAccountIds: Array<string | number> } {
-  const { finalConfig, skippedAccountIds } = mergeAccounts(
-    globalConfig,
-    deprecatedConfig
-  );
+export function mergeConfigAccounts(
+  toConfig: HubSpotConfig,
+  fromConfig: HubSpotConfig
+): {
+  configWithMergedAccounts: HubSpotConfig;
+  skippedAccountIds: Array<string | number>;
+} {
+  const { configWithMergedAccounts, skippedAccountIds } =
+    buildConfigWithMergedAccounts(toConfig, fromConfig);
 
-  writeGlobalConfigFile(finalConfig);
-  return { finalConfig, skippedAccountIds };
+  writeConfigFile(configWithMergedAccounts, getGlobalConfigFilePath());
+  return { configWithMergedAccounts, skippedAccountIds };
 }
