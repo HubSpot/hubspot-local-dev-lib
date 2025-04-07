e0a356cb1ef8cff953c4d9a75eb6d66be7b03156
Branden Rodgers <branden@rodgersworld.com>
2025-04-07 13:46:40 -0400

    feat: Add fire alarm api util (#253)

---
+++ api/fireAlarm.ts
@@ -0,0 +1,13 @@
+import { http } from '../http';
+import { FireAlarm } from '../types/FireAlarm';
+import { HubSpotPromise } from '../types/Http';
+
+const FIREALARM_API_AUTH_PATH = 'firealarm/v4/alarm';
+
+export function fetchFireAlarms(
+  accountId: number
+): HubSpotPromise<Array<FireAlarm>> {
+  return http.get<Array<FireAlarm>>(accountId, {
+    url: `${FIREALARM_API_AUTH_PATH}/hubspot-cli/${accountId}`,
+  });
+}
