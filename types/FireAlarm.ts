7d6df07359bbdf34030d4eabc6c8c87c5aa79d5d
Branden Rodgers <branden@rodgersworld.com>
2025-04-09 12:54:51 -0400

    chore: add a field to the fire alarm type (#256)

--- types/FireAlarm.ts
+++ types/FireAlarm.ts
@@ -2,6 +2,7 @@ export type FireAlarm = {
   id: number;
   title: string;
   message: string;
+  urlRegexPattern: string | null;
   querySelector: string | null;
   severity: string;
   createdAt: number;
