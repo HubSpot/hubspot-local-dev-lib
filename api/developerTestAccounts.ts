635c115d8ae918db0e5354edaa4b9a8855298ace
Branden Rodgers <branden@rodgersworld.com>
2025-07-09 17:52:52 -0400

    feat: Support v3 test account api (#279)
    * test account api utils
    
    * Support v3 test account api
    
    * export account level type

--- api/developerTestAccounts.ts
+++ api/developerTestAccounts.ts
@@ -6,12 +6,15 @@ import {
   DeveloperTestAccount,
   CreateDeveloperTestAccountResponse,
   FetchDeveloperTestAccountsResponse,
+  DeveloperTestAccountConfig,
+  CreateDeveloperTestAccountV3Response,
 } from '../types/developerTestAccounts';
 import { SANDBOX_TIMEOUT } from '../constants/api';
 import { Environment } from '../types/Config';
 import { HubSpotPromise } from '../types/Http';
 
 const TEST_ACCOUNTS_API_PATH = 'integrators/test-portals/v2';
+const TEST_ACCOUNTS_API_PATH_V3 = 'integrators/test-portals/v3';
 
 export function fetchDeveloperTestAccounts(
   accountId: number
@@ -23,19 +26,42 @@ export function fetchDeveloperTestAccounts(
 
 export function createDeveloperTestAccount(
   accountId: number,
-  accountName: string
-): HubSpotPromise<CreateDeveloperTestAccountResponse> {
+  accountInfo: string
+): HubSpotPromise<CreateDeveloperTestAccountResponse>;
+export function createDeveloperTestAccount(
+  accountId: number,
+  accountInfo: DeveloperTestAccountConfig
+): HubSpotPromise<CreateDeveloperTestAccountV3Response>;
+export function createDeveloperTestAccount(
+  accountId: number,
+  accountInfo: string | DeveloperTestAccountConfig
+): HubSpotPromise<
+  CreateDeveloperTestAccountResponse | CreateDeveloperTestAccountV3Response
+> {
+  if (typeof accountInfo === 'object') {
+    return http.post<CreateDeveloperTestAccountV3Response>(accountId, {
+      url: TEST_ACCOUNTS_API_PATH_V3,
+      data: accountInfo,
+      timeout: SANDBOX_TIMEOUT,
+    });
+  }
   return http.post<CreateDeveloperTestAccountResponse>(accountId, {
     url: TEST_ACCOUNTS_API_PATH,
-    data: { accountName, generatePersonalAccessKey: true }, // For CLI, generatePersonalAccessKey will always be true since we'll be saving the entry to the config
+    data: { accountName: accountInfo, generatePersonalAccessKey: true }, // For CLI, generatePersonalAccessKey will always be true since we'll be saving the entry to the config
     timeout: SANDBOX_TIMEOUT,
   });
 }
 
 export function deleteDeveloperTestAccount(
   accountId: number,
-  testAccountId: number
+  testAccountId: number,
+  useV3 = false
 ): HubSpotPromise<void> {
+  if (useV3) {
+    return http.delete(accountId, {
+      url: `${TEST_ACCOUNTS_API_PATH_V3}/${testAccountId}`,
+    });
+  }
   return http.delete(accountId, {
     url: `${TEST_ACCOUNTS_API_PATH}/${testAccountId}`,
   });
