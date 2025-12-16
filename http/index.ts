e57fece7372ba58966cdf31ec6b0b1169180b275
Chirag Chadha <chiragkchadha@gmail.com>
2025-12-16 16:25:27 -0500

    refactor: Move interceptor to client

--- http/index.ts
+++ http/index.ts
@@ -23,77 +23,9 @@ import {
   OAUTH_AUTH_METHOD,
   API_KEY_AUTH_METHOD,
 } from '../constants/auth';
-import { LOCALDEVAUTH_ACCESS_TOKEN_PATH } from '../api/localDevAuth';
-import { FIREALARM_API_AUTH_PATH } from '../api/fireAlarm';
-import * as util from 'util';
-import { CMS_CLI_USAGE_PATH, VSCODE_USAGE_PATH } from '../lib/trackUsage';
 import { httpClient } from './client';
 
 const i18nKey = 'http.index';
-
-const IGNORE_URLS_NETWORK_DEBUG = [
-  LOCALDEVAUTH_ACCESS_TOKEN_PATH,
-  CMS_CLI_USAGE_PATH,
-  VSCODE_USAGE_PATH,
-  FIREALARM_API_AUTH_PATH,
-];
-
-function logRequest(response: AxiosResponse) {
-  try {
-    if (!process.env.HUBSPOT_NETWORK_LOGGING) {
-      return;
-    }
-
-    if (
-      !process.env.HUBSPOT_DEBUG_LOGGING_VERBOSE &&
-      IGNORE_URLS_NETWORK_DEBUG.some(
-        url => response?.config?.url && response.config.url.includes(url)
-      )
-    ) {
-      return;
-    }
-
-    logger.debug(
-      util.inspect(
-        {
-          method: response.config.method,
-          baseURL: response.config.baseURL,
-          url: response.config.url,
-          data: response.data,
-          status: response.status,
-        },
-        false,
-        null,
-        true
-      )
-    );
-  } catch (error) {
-    // Ignore any errors that occur while logging the response
-  }
-}
-
-// Register interceptor on our isolated instance
-httpClient.interceptors.response.use(
-  (response: AxiosResponse) => {
-    logRequest(response);
-    return response;
-  },
-  error => {
-    try {
-      if (isAxiosError(error) && error.response) {
-        logRequest(error.response);
-      }
-    } catch (e) {
-      // Ignore any errors that occur while logging the response
-    }
-
-    // Wrap all axios errors in our own Error class. Attach the error
-    // as the cause for the new error, so we maintain the stack trace
-    return Promise.reject(
-      new HubSpotHttpError(error.message, { cause: error })
-    );
-  }
-);
 
 export function addUserAgentHeader(key: string, value: string) {
   USER_AGENTS[key] = value;
