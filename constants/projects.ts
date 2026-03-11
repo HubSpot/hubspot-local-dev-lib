6f3a4204c46de8d4c4fecb83f1b0840336add7a9
Joe Yeager <jyeager@hubspot.com>
2026-03-11 07:21:53 -0700

    Change to beta

--- constants/projects.ts
+++ constants/projects.ts
@@ -9,6 +9,6 @@ export const PLATFORM_VERSIONS = {
   v2023_2: '2023.2',
   v2025_1: '2025.1',
   v2025_2: '2025.2',
-  v2026_3: '2026.03',
+  v2026_3_beta: '2026.03-beta',
   unstable: 'unstable',
 };
